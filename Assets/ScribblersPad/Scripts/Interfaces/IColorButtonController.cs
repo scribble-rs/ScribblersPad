fb591172d6f06ddf1644b3237d11a697c7f4ca26
BigETI <bigeti.ethem@gmail.com>
2021-02-17 20:38:19 +0100

    Added a simple color picker. Updated game scene UI.

---
+++ Assets/ScribblersPad/Scripts/Interfaces/IColorButtonController.cs
@@ -0,0 +1,31 @@
+﻿using ScribblersPad.Controllers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// Scribble.rs Pad namespace
+/// </summary>
+namespace ScribblersPad
+{
+    /// <summary>
+    /// An interface that represents a color button controller script
+    /// </summary>
+    public interface IColorButtonController : IBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        /// <summary>
+        /// Color
+        /// </summary>
+        Color Color { get; set; }
+
+        /// <summary>
+        /// Color image
+        /// </summary>
+        Image ColorImage { get; set; }
+
+        /// <summary>
+        /// Color picker controller
+        /// </summary>
+        ColorPickerControllerScript ColorPickerController { get; set; }
+    }
+}
